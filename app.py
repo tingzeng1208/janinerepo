3d8b0574a694c27ab2cfa94ea6de70d0783d9813
ja9-z <janine.zeng.6@gmail.com>
2025-03-29 14:48:39 -0400

    basic function + df

--- app.py
+++ app.py
@@ -1,9 +1,32 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, redirect, url_for, request, render_template, flash
+import pandas as pd
+import numpy as np
+
+df1=pd.read_csv("resources/tmdb_5000_credits.csv")
+df2=pd.read_csv("resources/tmdb_5000_movies.csv")
+
+
+# Flask constructor
+app = Flask(__name__)   
+
 
 @app.route("/")
-
 def home():
-    return "Hello Worlddd"
+    return "test"
+
+@app.route("/login", methods = ["POST", "GET"])
+def login():
+    if(request.method == "POST"):
+        #can check if request.form["nm"] not blank, do more stuff with it
+        user = request.form["nm"]
+        return redirect(url_for("user", usr = user))
+    else:
+        return render_template("login.html")
+
+@app.route("/<usr>")
+def user(usr):
     
-app.run(host = "0.0.0.0", port = 80)
+    return f"<h1>{usr}</h1>"
+
+if __name__=='__main__':
+    app.run(debug = True)
